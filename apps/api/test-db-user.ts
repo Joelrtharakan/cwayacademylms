a809fcdff7edc46d984b1c44df3b6606767a8400
Joel R Tharkan <joelrtharakan@gmail.com>
2026-07-07 00:15:14 +0530

    chore: remove test scripts containing database credentials

--- apps/api/test-db-user.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany({
-    where: { role: 'INSTRUCTOR' }
-  });
-  console.log("Instructors:", users.map(u => ({ id: u.id, email: u.email, name: u.name })));
-}
-main().finally(() => prisma.$disconnect());
